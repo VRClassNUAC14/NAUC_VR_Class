a0c5fe5559fe355bb627d94d3fa20d21fee01973
HeyJoeLang <heyjoelang@gmail.com>
2024-04-20 08:50:14 -0400

    Migrating Coding Lesson To Unique Folder

---
+++ Assets/CodingLesson/Scripts/OnTrigger_Explosion.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class OnTrigger_Explosion : MonoBehaviour      
+{
+
+}
--- Assets/OnTrigger_Explosion.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class OnTrigger_Explosion : MonoBehaviour // Name of the class and script         
-{
-
-}
